1b1011d36d20592ef28a7eda968bd6273417ee36
FrickTzy <frickkun@gmail.com>
2023-11-24 13:29:18 +0800

    Added song!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Main:
     def __init__(self):
         self.music = Music()
         self.timer = Timer()
-        self.chosen_song = "Special"
+        self.chosen_song = "Reviver"
         self.play_tracker = PlayTracker(self.chosen_song)
         self.play_window = PlayWindow(self.music, self.timer, MapManager, self.play_tracker, self.chosen_song)
 
