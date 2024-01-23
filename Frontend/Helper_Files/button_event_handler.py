86b69bdb64fadf004d2019aa51813d00707b830d
FrickTzy <frickkun@gmail.com>
2024-01-23 19:31:11 +0800

    Cleaned up a bit.

--- Frontend/Helper_Files/button_event_handler.py
+++ Frontend/Helper_Files/button_event_handler.py
@@ -29,29 +29,15 @@ class ButtonEventHandler:
         :return bool:
         """
 
-        x_1, y_1 = starting_pos_1
-        x_2, y_2 = starting_pos_2
-
         ending_pos_x_1, ending_pos_y_1 = cls.__get_ending_pos(starting_pos=starting_pos_1, size=size_1)
         ending_pos_x_2, ending_pos_y_2 = cls.__get_ending_pos(starting_pos=starting_pos_2, size=size_2)
 
-        second_object_x_inside_first_object_x = x_1 <= x_2
-        second_object_x_inside_first_object_ending_x = x_2 <= ending_pos_x_1
-
-        second_object_y_inside_first_object_y = y_1 <= y_2
-        second_object_y_inside_first_object_ending_y = y_2 <= ending_pos_y_2
-
         object_2_inside_object_1 = ending_pos_y_1 >= ending_pos_y_2 and ending_pos_x_1 >= ending_pos_x_2
 
-        if not second_object_x_inside_first_object_x:
+        if not object_2_inside_object_1:
             return False
-        elif not second_object_x_inside_first_object_ending_x:
-            return False
-        elif not second_object_y_inside_first_object_y:
-            return False
-        elif not second_object_y_inside_first_object_ending_y:
-            return False
-        elif not object_2_inside_object_1:
+        if not cls.__check_if_an_object_comes_contact_with_another_object(starting_pos_1=starting_pos_1, size_1=size_1,
+                                                                          starting_pos_2=starting_pos_2, size_2=size_2):
             return False
 
         return True
@@ -68,6 +54,33 @@ class ButtonEventHandler:
         :return bool:
         """
 
+        ending_pos_x_1, ending_pos_y_1 = cls.__get_ending_pos(starting_pos=starting_pos_1, size=size_1)
+        x_2, y_2 = starting_pos_2
+
+        object_2_crosses_object_1 = ending_pos_y_1 >= y_2 and ending_pos_x_1 >= x_2
+
+        if not cls.__check_if_an_object_comes_contact_with_another_object(starting_pos_1=starting_pos_1, size_1=size_1,
+                                                                          starting_pos_2=starting_pos_2, size_2=size_2):
+            return False
+        if not object_2_crosses_object_1:
+            return False
+
+        return True
+
+    @classmethod
+    def __check_if_an_object_comes_contact_with_another_object(cls, starting_pos_1: tuple[int, int],
+                                                               size_1: tuple[int, int],
+                                                               starting_pos_2: tuple[int, int],
+                                                               size_2: tuple[int, int]):
+        """
+               First object is the bigger object and the second object is the smaller one
+               :param starting_pos_1:
+               :param size_1:
+               :param starting_pos_2:
+               :param size_2:
+               :return bool:
+               """
+
         x_1, y_1 = starting_pos_1
         x_2, y_2 = starting_pos_2
 
@@ -80,8 +93,6 @@ class ButtonEventHandler:
         second_object_y_inside_first_object_y = y_1 <= y_2
         second_object_y_inside_first_object_ending_y = y_2 <= ending_pos_y_2
 
-        object_2_crosses_object_1 = ending_pos_y_1 >= y_2 and ending_pos_x_1 >= x_2
-
         if not second_object_x_inside_first_object_x:
             return False
         elif not second_object_x_inside_first_object_ending_x:
@@ -90,9 +101,6 @@ class ButtonEventHandler:
             return False
         elif not second_object_y_inside_first_object_ending_y:
             return False
-        elif not object_2_crosses_object_1:
-            return False
-
         return True
 
     @staticmethod
